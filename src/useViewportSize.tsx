f6f132dd1a326c72e09156b21bc9c272101914f5
Каспаров Владислав Маратович <vlad-kasparov@mail.ru>
2025-01-23 19:12:14 +0300

    feat: 4

--- src/useViewportSize.tsx
+++ src/useViewportSize.tsx
@@ -1,1 +1,26 @@
+import { useCallback, useState } from "react"
+import { useWindowEvent } from "./Demo"
 
+interface ViewportSize {
+	width: number
+	height: number
+}
+
+
+export const useViewportSize = () => {
+	const [viewportSize, setViewportSize] = useState<ViewportSize>({
+		width: window.innerWidth,
+		height: window.innerHeight
+	})
+
+	const showViewportSize = useCallback(() => {
+		setViewportSize({
+			width: window.innerWidth,
+			height: window.innerHeight
+		})
+	}, [])
+
+	useWindowEvent("resize", showViewportSize)
+
+	return {...viewportSize}
+}
